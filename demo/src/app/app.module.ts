72357712a9311f4244ec074766ae01b457b1ba75
twp0217 <tu.wenpan@qq.com>
2017-05-24 16:18:32 +0800

    feat(demo): 新增demo工程

---
+++ demo/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import { NgxPasswordStrengthModule } from 'ngx-password-strength';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    NgxPasswordStrengthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
